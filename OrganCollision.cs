f70a58c15e7e90cfe39121af8cd0ddf221a545dc
vlgonz <134500042+vlgonz@users.noreply.github.com>
2023-05-24 20:40:17 +0200

    Add files via upload
    Version with the names of the organs included. So, names and colliders work!!!!!!!!! :)

--- OrganCollision.cs
+++ OrganCollision.cs
@@ -1,44 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
 
 public class OrganCollision : MonoBehaviour
 {
+    private static List<string> activeGameObjects = new List<string>();
+    private static TextMeshProUGUI organTextBox;
+
     void Start()
     {
-        // Disable the MeshRenderer component at the start
         MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.enabled = false;
-        
+
+        if (organTextBox == null)
+        {
+            GameObject canvas = GameObject.Find("Canvas");
+            organTextBox = canvas.transform.Find("Organ text box").GetComponent<TextMeshProUGUI>();
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Enter");
             gameObject.GetComponent<MeshRenderer>().enabled = true;
-        }
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        // Check if the collision was with the capsule game object
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("Stay");
-            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            AddGameObjectName(gameObject.name);
+            UpdateTextBox();
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        // Check if the collision was with the capsule game object
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Exit");
             gameObject.GetComponent<MeshRenderer>().enabled = false;
+            RemoveGameObjectName(gameObject.name);
+            UpdateTextBox();
         }
+    }
+
+    void AddGameObjectName(string gameObjectName)
+    {
+        if (!activeGameObjects.Contains(gameObjectName))
+        {
+            activeGameObjects.Add(gameObjectName);
+        }
+    }
+
+    void RemoveGameObjectName(string gameObjectName)
+    {
+        activeGameObjects.Remove(gameObjectName);
+    }
+
+    void UpdateTextBox()
+    {
+        organTextBox.text = string.Join("\n", activeGameObjects);
     }
 }
